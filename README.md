3906ea3870c12e828d75cd181855c6857bcd22a4
Adnan Siddiqui <60623435+adnansid99@users.noreply.github.com>
2022-07-24 01:07:49 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Odd-Even Number Checker
+
+**How to Run the numcheck.py**
+
+```bash
+$ git clone https://github.com/adnansid99/Odd-Even-Check.git
+
+$ cd Odd-Even-Check
+
+$ python numcheck.py
+```
---
+++ numcheck.py
@@ -0,0 +1,8 @@
+# Check the number is Odd or Even https://github.com.adnansid99
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(num, "is an Even number")
+else:
+    print(num, "is an Odd number")
